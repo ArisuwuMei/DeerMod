299849cacf27bbe62c5ebb51f4a3ee3105ea80d8
ArisuwuMei <arisuwu.mei@gmail.com>
2025-02-12 18:50:17 +0100

    Remove unused

--- common/src/main/java/mei/arisuwu/deermod/Mod.java
+++ common/src/main/java/mei/arisuwu/deermod/Mod.java
@@ -3,13 +3,4 @@ package mei.arisuwu.deermod;
 public final class Mod
 {
     public static final String MOD_ID = "deermod";
-
-    public static void init()
-    {
-        //ModEntities.init();
-        //ModFoodComponents.init();
-        //ModLootTables.init();
-        //ModItems.init();
-        //ModTags.init();
-    }
 }
