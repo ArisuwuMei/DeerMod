475d32f7a7712f0502f326bcfb0205c61c6a6a76
ArisuwuMei <arisuwu.mei@gmail.com>
2025-04-04 04:42:42 +0200

    Remove unused imports

--- neoforge/src/main/java/mei/arisuwu/deermod/neoforge/NeoforgeModItems.java
+++ neoforge/src/main/java/mei/arisuwu/deermod/neoforge/NeoforgeModItems.java
@@ -4,10 +4,8 @@ import mei.arisuwu.deermod.ModResourceLocation;
 import mei.arisuwu.deermod.ModItems;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Function;
