f8a08503f62d1fe49e323fc3fb160eceae2d02b9
ArisuwuMei <arisuwu.mei@gmail.com>
2025-04-09 03:30:19 +0200

    Add Datagen

---
+++ common/src/datagen/java/mei/arisuwu/deermod/datagen/ModDataGenerator.java
@@ -0,0 +1,13 @@
+package mei.arisuwu.deermod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModDataGenerator implements DataGeneratorEntrypoint
+{
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator)
+    {
+
+    }
+}
---
+++ common/src/datagen/java/mei/arisuwu/deermod/datagen/ModInit.java
@@ -0,0 +1,18 @@
+package mei.arisuwu.deermod.datagen;
+
+import mei.arisuwu.deermod.ModEntities;
+import mei.arisuwu.deermod.ModItems;
+import mei.arisuwu.deermod.entity.deer.DeerEntity;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+
+public class ModInit implements ModInitializer
+{
+    @Override
+    public void onInitialize()
+    {
+        new ModEntities();
+        new ModItems();
+        FabricDefaultAttributeRegistry.register(ModEntities.DEER.get(), DeerEntity.createAttributes());
+    }
+}
