bee6e297549fa06af9ec999806f4a680aa8dbedb
ArisuwuMei <arisuwu.mei@gmail.com>
2025-09-06 18:36:05 +0200

    Change spawn predicate for deers from Animal to Mob

--- fabric/src/main/java/mei/arisuwu/deermod/fabric/FabricMod.java
+++ fabric/src/main/java/mei/arisuwu/deermod/fabric/FabricMod.java
@@ -7,11 +7,12 @@ import mei.arisuwu.deermod.entity.deer.DeerEntity;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.SpawnPlacementTypes;
 import net.minecraft.world.entity.SpawnPlacements;
-import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.level.levelgen.Heightmap;
 
 
@@ -43,7 +44,7 @@ public final class FabricMod implements ModInitializer
 
         SpawnPlacements.register(
             ModEntities.DEER.get(), SpawnPlacementTypes.ON_GROUND,
-            Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Animal::checkAnimalSpawnRules
+            Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Mob::checkMobSpawnRules
         );
     }
 }
